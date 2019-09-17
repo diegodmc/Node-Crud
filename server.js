4a1f2dfcc509b055198da574d29f349ebfe5eb01
pl5786 <Diego.costa@external.engie.com>
2019-09-16 22:23:48 -0300

    Instalação do required-dir

--- server.js
+++ server.js
@@ -2,15 +2,29 @@ const express = require('express');
 
 const mongoose = require('mongoose');
 
+const requireDir = require('require-dir');
+
 //Iniciando o app
 const app = express();
 
 //Iniciando o DB
-mongoose.connect("mongodb:localhost:27017/nodepai", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/nodeapi", { 
+                                                      useNewUrlParser: true 
+                                                    }
+                );
+
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
 
 //Primeira Rota
 app.get('/',(req, res) => { 
-                            res.send('Hello!!');
+                           Product.create({
+                                            title: 'Node', 
+                                            description: 'Node JS',
+                                            url: 'https://nodejs.org/en/'
+                           });
+                            return res.send('Hello!!');
                           }
        );
 
